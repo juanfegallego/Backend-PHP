ace52239dca99a38e8228e32efdad0925206ae4c
juanfegallego <juanfegallegodj@gmail.com>
2021-07-13 19:27:27 +0200

    comments model created

--- app/Models/Comments.php
+++ app/Models/Comments.php
@@ -7,5 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comments extends Model
 {
-    use HasFactory;
+    public function user (){
+        return $this -> belongsTo (User::class);
+
+        
+    }
+
+    public function party (){
+        return $this -> belongsTo (Party::class);
+
+        
+    }
 }
