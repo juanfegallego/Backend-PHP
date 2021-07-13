8f5ed32ea5b07b51d229f44519aa7b6c93fb1c41
juanfegallego <juanfegallegodj@gmail.com>
2021-07-13 19:25:13 +0200

    party model created

--- app/Models/Party.php
+++ app/Models/Party.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Party extends Model
 {
     use HasFactory;
+
+    public function message (){
+        return $this -> hasMany(Comments::class);
+    }
+
+    public function partyuser (){
+        return $this -> belongsTo(PartyUser::class);
+    }
 }
