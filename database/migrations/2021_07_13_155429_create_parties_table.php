676bb3441ec1bd965446011e206e0001046a61cf
juanfegallego <juanfegallegodj@gmail.com>
2021-07-13 18:17:17 +0200

    kick off

---
+++ database/migrations/2021_07_13_155429_create_parties_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parties', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->foreignId('game_id')->references('id')->on('games');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parties');
+    }
+}
