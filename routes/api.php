ba186c6c53cef948586830abfc360d68f1570b73
juanfegallego <juanfegallegodj@gmail.com>
2021-07-15 17:21:44 +0200

    register and login routes created

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PassportAuthController;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+
+
+Route::middleware('auth:api')->group(function(){
+    Route::resource('comment', CommentController::class);
 });
