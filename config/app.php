82afb75f2217e36f1df54aeaa9d72d767d0b57bb
juanfegallego <juanfegallegodj@gmail.com>
2021-07-15 18:19:39 +0200

    AppServiceProvide configured

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class,
 
+        
+
         /*
          * Package Service Providers...
          */
@@ -229,6 +231,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
 
     ],
 
