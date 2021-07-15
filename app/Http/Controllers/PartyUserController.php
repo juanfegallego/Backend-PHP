55612f5f6c28fe880a71cca73829a7db055e3542
juanfegallego <juanfegallegodj@gmail.com>
2021-07-15 17:18:15 +0200

    PartyUserController created

---
+++ app/Http/Controllers/PartyUserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PartyUserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PartyUser  $partyUser
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PartyUser $partyUser)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PartyUser  $partyUser
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PartyUser $partyUser)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PartyUser  $partyUser
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PartyUser $partyUser)
+    {
+        //
+    }
+}
