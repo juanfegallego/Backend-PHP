94c12b8df4b321235e7c3ea273905121b4c394c8
juanfegallego <juanfegallegodj@gmail.com>
2021-07-15 17:19:17 +0200

    Comment.php created

---
+++ app/Models/Comment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{   
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+    //  Relacion de un comentario a user
+    public function user (){
+        return $this -> belongsTo (User::class);
+
+        
+    }
+    //  Relacion de un comentario a una party
+    public function party (){
+        return $this -> belongsTo (Party::class);
+
+        
+    }
+}
