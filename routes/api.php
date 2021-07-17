5694bbc5419dcf200d591f8d0648b672d6f26fa8
juanfegallego <juanfegallegodj@gmail.com>
2021-07-17 17:23:15 +0200

    Game routes created

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PassportAuthController;
 use App\Http\Controllers\CommentController;
+use App\Http\Controllers\GameController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,12 @@ Route::post('login', [PassportAuthController::class, 'login']);
 
 
 Route::middleware('auth:api')->group(function(){
+
     Route::resource('comment', CommentController::class);
+
+    //CRUD of Game
+    Route::post('game', [GameController::class, 'store']);
+    Route::put('game', [GameController::class, 'update']);
+    Route::get('game/{id}', [GameController::class, 'show']);
+    Route::delete('game', [GameController::class, 'destroy']);
 });
